fce64d29456533d80e80af24a62591dd9678d776
Dron245 <Dron245@yandex.ru>
2022-12-26 15:52:42 +0300

    2612R

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,16 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+document.addEventListener('click', documentactions)
+
+function documentactions (e) {
+	const target = e.target
+	//e.preventdefault();
+	if(target.closest('.header__city')){
+		document.querySelector('.header__city-questions').classList.add('_active')
+	}
+	if (!target.closest('.header__city')) {
+		document.querySelector('.header__city-questions').classList.remove('_active')
+	}
+}
