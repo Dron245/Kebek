939670fc65ad1c30d504bee4e14eecc379b111aa
Dron245 <Dron245@yandex.ru>
2022-12-30 13:42:55 +0300

    3012R

--- src/js/files/forms/range.js
+++ src/js/files/forms/range.js
@@ -8,35 +8,39 @@ import * as noUiSlider from 'nouislider';
 // import 'nouislider/dist/nouislider.css';
 
 export function rangeInit() {
-	const priceSlider = document.querySelector('#range');
-	if (priceSlider) {
-		let textFrom = priceSlider.getAttribute('data-from');
-		let textTo = priceSlider.getAttribute('data-to');
-		noUiSlider.create(priceSlider, {
-			start: 0, // [0,200000]
-			connect: [true, false],
-			range: {
-				'min': [0],
-				'max': [200000]
+	const priceSlider = document.querySelectorAll('.range');
+	priceSlider.forEach(element => {
+		if (element) {
+			let textFrom = element.getAttribute('data-from');
+			let textTo = element.getAttribute('data-to');
+			noUiSlider.create(element, {
+				start: 1,
+				step: 1,
+				tooltips: [wNumb({decimals: 1})],
+				connect: [true, false],
+				range: {
+					'min': [1],
+					'max': [10]
+				}
+			});
+			/*
+			const priceStart = document.getElementById('price-start');
+			const priceEnd = document.getElementById('price-end');
+			priceStart.addEventListener('change', setPriceValues);
+			priceEnd.addEventListener('change', setPriceValues);
+			*/
+			function setPriceValues() {
+				let priceStartValue;
+				let priceEndValue;
+				if (priceStart.value != '') {
+					priceStartValue = priceStart.value;
+				}
+				if (priceEnd.value != '') {
+					priceEndValue = priceEnd.value;
+				}
+				priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
 			}
-		});
-		/*
-		const priceStart = document.getElementById('price-start');
-		const priceEnd = document.getElementById('price-end');
-		priceStart.addEventListener('change', setPriceValues);
-		priceEnd.addEventListener('change', setPriceValues);
-		*/
-		function setPriceValues() {
-			let priceStartValue;
-			let priceEndValue;
-			if (priceStart.value != '') {
-				priceStartValue = priceStart.value;
-			}
-			if (priceEnd.value != '') {
-				priceEndValue = priceEnd.value;
-			}
-			priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
 		}
-	}
+	});
 }
 rangeInit();
